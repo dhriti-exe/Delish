e4f89b19b1c1320e46cf00742ef2fd2096491084
dhriti-exe <sahadhritishree@gmail.com>
2024-06-22 04:16:32 +0530

    Frontend Added!

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favourite food.</h2>
+            <p>
+                Embark on a culinary adventure! Explore a world of flavors you never
+                knew existed. Order your perfect meal today and discover a taste
+                sensation that will leave you wanting more.
+            </p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
