d13062d4d70e366337164dc5bdd706e4a239edf2
dhriti-exe <sahadhritishree@gmail.com>
2024-06-26 01:07:23 +0530

    Stripe gateway integrated!

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,56 +1,99 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import "./PlaceOrder.css";
 import { StoreContext } from "../../context/StoreContext";
+// import { toast } from 'react-toastify';
+import axios from 'axios';
 
 const PlaceOrder = () => {
-  const { getTotalCartAmount } = useContext(StoreContext);
+	const { getTotalCartAmount, token, food_list, cartItems, url  } = useContext(StoreContext);
+	const [data, setData] = useState({
+		firstName: "",
+		lastName: "",
+		email: "",
+		street: "",
+		city: "",
+		state: "",
+		zipcode: "",
+		country: "",
+		phone: ""
+	})
 
-  return (
-    <form className="place-order">
-      <div className="place-order-left">
-        <p className="title">Delivery Information</p>
-        <div className="multi-fields">
-          <input type="text" placeholder="First name" />
-          <input type="text" placeholder="Last name" />
-        </div>
-        <input type="email" placeholder="Email address" />
-        <input type="text" placeholder="Street" />
-        <div className="multi-fields">
-          <input type="text" placeholder="District" />
-          <input type="text" placeholder="City" />
-        </div>
-        <div className="multi-fields">
-          <input type="text" placeholder="Zip code" />
-          <input type="text" placeholder="Country" />
-        </div>
-        <input type="text" placeholder="Phone" />
-      </div>
-      <div className="place-order-right">
-        <div className="cart-total">
-          <h2>Cart Total</h2>
-          <div>
-            <div className="cart-total-details">
-              <p>Subtotal</p>
-              <p>${getTotalCartAmount()}</p>
-            </div>
-            <hr />
-            <div className="cart-total-details">
-              <p>Delivery fee</p>
-              <p>${getTotalCartAmount() === 0 ? 0 : 2}</p>
-            </div>
-            <hr />
-            <div className="cart-total-details">
-              <b>Total</b>
-              <b>
-                ${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}
-              </b>
-            </div>
-          </div>
-          <button>PROCEED TO PAYMENT</button>
-        </div>
-      </div>
-    </form>
-  );
+	const onChangeHandler = (event) => {
+		const name = event.target.name
+		const value = event.target.value
+		setData(data => ({ ...data, [name]: value }))
+	}
+
+	const placeOrder = async (event) => {
+		event.preventDefault()
+		let orderItems = [];
+		food_list.map(((item) => {
+			if (cartItems[item._id] > 0) {
+				let itemInfo = item;
+				itemInfo["quantity"] = cartItems[item._id];
+				orderItems.push(itemInfo)
+			}
+		}))
+		let orderData = {
+			address: data,
+			items: orderItems,
+			amount: getTotalCartAmount() + 2,
+		}
+		let response = await axios.post(url + "/api/order/place", orderData, { headers: { token } });
+		if (response.data.success) {
+			const { session_url } = response.data;
+			window.location.replace(session_url);
+		}
+		else {
+			alert("Error");
+		}
+	}
+	return (
+		<form onSubmit={placeOrder} className="place-order">
+			<div className="place-order-left">
+				<p className="title">Delivery Information</p>
+				<div className="multi-fields">
+				<input required type="text" name='firstName' onChange={onChangeHandler} value={data.firstName} placeholder="First name" />
+				<input required type="text" name='lastName' onChange={onChangeHandler} value={data.lastName} placeholder="Last name" />
+				</div>
+				<input required type="email" name='email' onChange={onChangeHandler} value={data.email} placeholder="Email address" />
+				<input required type="text" name='street' onChange={onChangeHandler} value={data.street} placeholder="Street" />
+				<div className="multi-fields">
+				<input required type="text" name='city' onChange={onChangeHandler} value={data.city} placeholder="City" />
+				<input required type="text" name='state' onChange={onChangeHandler} value={data.state} placeholder='State' />
+				</div>
+				<div className="multi-fields">
+				<input required type="text" name='zipcode' onChange={onChangeHandler} value={data.zipcode} placeholder='Zip code' />
+				<input required type="text" name='country' onChange={onChangeHandler} value={data.country} placeholder='Country' />
+				</div>
+				<input required type="text" name='phone' onChange={onChangeHandler} value={data.phone} placeholder="Phone" />
+			</div>
+			<div className="place-order-right">
+				<div className="cart-total">
+				<h2>Cart Total</h2>
+				<div>
+					<div className="cart-total-details">
+					<p>Subtotal</p>
+					<p>${getTotalCartAmount()}</p>
+					</div>
+					<hr />
+					<div className="cart-total-details">
+					<p>Delivery fee</p>
+					<p>${getTotalCartAmount() === 0 ? 0 : 2}</p>
+					</div>
+					<hr />
+					<div className="cart-total-details">
+					<b>Total</b>
+					<b>
+						${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}
+					</b>
+					</div>
+				</div>
+				<button type="submit">PROCEED TO PAYMENT</button>
+				</div>
+			</div>
+		</form>
+	);
 };
 
 export default PlaceOrder;
